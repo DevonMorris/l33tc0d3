b6fc36b58f0d3353df0b727c154d7c1a0890d717
Devon Morris <devonmorris1992@gmail.com>
2020-05-21 01:17:25 +0000

    hamming weight

---
+++ cpp/191.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int hammingWeight(uint32_t n) {
+      int hamming_dist = 0;
+      for(size_t i = 0; i < 32; ++i)
+      {
+        uint32_t digit = (1ul << i) & n;
+        if (digit != 0)
+        {
+          ++hamming_dist;
+        }
+      }
+      return hamming_dist;
+    }
+};
