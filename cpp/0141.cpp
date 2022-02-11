06d92ba953700161b418b7783e9be07ceaf77088
Devon Morris <devonmorris1992@gmail.com>
2022-02-11 07:47:11 -0500

    tortoise and hare

--- cpp/0141.cpp
+++ cpp/0141.cpp
@@ -15,4 +15,38 @@ public:
     }
 };
 
-// Constant Space Solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) {
+            return false;
+        }
+        if (head->next == nullptr) {
+            return false;
+        }
+        ListNode* tortoise = head;
+        ListNode* hare = head->next;
+
+        while (tortoise != nullptr && hare != nullptr) {
+            if (tortoise == hare) {
+                return true;
+            }
+
+            tortoise = tortoise->next;
+            hare = hare->next;
+            if (hare == nullptr) {
+                return false;
+            }
+            hare = hare->next;
+        }
+        return false;
+    }
+};
