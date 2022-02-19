43cc187e61606bbea53324638e11301ea0d0636f
Devon Morris <devonmorris1992@gmail.com>
2022-02-19 15:08:32 -0500

    linked list stuff again

--- cpp/0002.cpp
+++ cpp/0002.cpp
@@ -32,3 +32,62 @@ public:
         return first;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr && l2 == nullptr) {
+            return nullptr;
+        }
+        int carry = 0;
+        int new_val = l1->val + l2->val;
+        if (l1->val + l2->val > 9) {
+            carry = new_val / 10;
+            new_val = new_val % 10;
+        }
+        l1 = l1->next;
+        l2 = l2->next;
+        ListNode* head = new ListNode(new_val);
+        ListNode* prev_node = head;
+        ListNode* node = nullptr;
+
+        while (l1 != nullptr || l2 != nullptr) {
+            int val1 = l1 == nullptr ? 0 : l1->val;
+            int val2 = l2 == nullptr ? 0 : l2->val;
+            node = new ListNode(0);
+            prev_node->next = node;
+            new_val = val1 + val2 + carry;
+            carry = 0;
+            if (new_val > 9) {
+                carry = new_val / 10;
+                new_val = new_val % 10;
+            }
+            node->val = new_val;
+            prev_node = node;
+            if (l1 != nullptr) {
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                l2 = l2->next;
+            }
+        }
+
+        if (carry != 0) {
+            node = new ListNode(carry);
+            prev_node->next = node;
+            prev_node = node;
+        }
+
+        return head;
+    }
+};
